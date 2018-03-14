3a0caff2cabbe43c797034fd85c558de878e1fa0
Caryl Westerberg <cjwest@acm.org>
2018-03-13 19:12:15 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function ($) {
+
+  /*
+   *  My script code comment
+   */
+  Drupal.behaviors.collection = {
+    attach: function (context) {
+      // My code here...
+    }
+  };
+
+})(jQuery);
